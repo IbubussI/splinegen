0225c71d28c3527e73a60b8e6957c6a5eba29d82
IbubussI <80070101+IbubussI@users.noreply.github.com>
2025-09-01 01:42:38 +0300

    Hide console on windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,16 @@
 #include <SFML/Graphics.hpp>
 #include "app.h"
 
+#ifdef _WIN32
+    #define _WIN32_WINNT 0x0500
+    #include <windows.h>
+#endif
+
 int main() {
+#ifdef _WIN32
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+#endif
+
     using namespace sf;
     using namespace TV::Math;
     RenderWindow window(VideoMode(Vector2u{1000, 1000}), "Spline Generator", Style::Titlebar | Style::Close);
